a12ef3a307589e1b3e0e3ab7412c68645889a937
Allen <allen.davisswing@gmail.com>
2024-08-22 19:05:54 -0400

    just get rid of it all

--- server/server/settings.py
+++ server/server/settings.py
@@ -53,7 +53,7 @@ if DEBUG:
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-	'corsheaders.middleware.CorsMiddleware',
+	# 'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
